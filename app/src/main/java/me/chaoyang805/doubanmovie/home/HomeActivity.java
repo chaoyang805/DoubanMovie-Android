22093bc2ac1398a8956f1e79cd9f8ce9adb84348
chaoyang805 <zhangchaoyang805@gmail.com>
2016-10-19 23:33:43 +0800

    complete navigation drawer

--- app/src/main/java/me/chaoyang805/doubanmovie/home/HomeActivity.java
+++ app/src/main/java/me/chaoyang805/doubanmovie/home/HomeActivity.java
@@ -50,83 +50,6 @@ public class HomeActivity extends ToolbarActivity
             ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), homeFragment, R.id.content_frame);
         }
         mPresenter = new HomePresenter(new MoviesRepository(), homeFragment);
-        testGson();
-    }
-
-    private void testGson() {
-
-        String jsonString = "{\n" +
-            "    \"rating\": {\n" +
-            "        \"max\": 10,\n" +
-            "        \"average\": 8.2,\n" +
-            "        \"stars\": \"40\",\n" +
-            "        \"min\": 0\n" +
-            "    },\n" +
-            "    \"genres\": [\n" +
-            "        \"动作\",\n" +
-            "        \"犯罪\"\n" +
-            "    ],\n" +
-            "    \"title\": \"湄公河行动\",\n" +
-            "    \"casts\": [\n" +
-            "        {\n" +
-            "            \"alt\": \"https://movie.douban.com/celebrity/1274319/\",\n" +
-            "            \"avatars\": {\n" +
-            "                \"small\": \"https://img1.doubanio.com/img/celebrity/small/49047.jpg\",\n" +
-            "                \"large\": \"https://img1.doubanio.com/img/celebrity/large/49047.jpg\",\n" +
-            "                \"medium\": \"https://img1.doubanio.com/img/celebrity/medium/49047.jpg\"\n" +
-            "            },\n" +
-            "            \"name\": \"张涵予\",\n" +
-            "            \"id\": \"1274319\"\n" +
-            "        },\n" +
-            "        {\n" +
-            "            \"alt\": \"https://movie.douban.com/celebrity/1013782/\",\n" +
-            "            \"avatars\": {\n" +
-            "                \"small\": \"https://img3.doubanio.com/img/celebrity/small/1368156632.65.jpg\",\n" +
-            "                \"large\": \"https://img3.doubanio.com/img/celebrity/large/1368156632.65.jpg\",\n" +
-            "                \"medium\": \"https://img3.doubanio.com/img/celebrity/medium/1368156632.65.jpg\"\n" +
-            "            },\n" +
-            "            \"name\": \"彭于晏\",\n" +
-            "            \"id\": \"1013782\"\n" +
-            "        },\n" +
-            "        {\n" +
-            "            \"alt\": \"https://movie.douban.com/celebrity/1274640/\",\n" +
-            "            \"avatars\": {\n" +
-            "                \"small\": \"https://img1.doubanio.com/img/celebrity/small/5979.jpg\",\n" +
-            "                \"large\": \"https://img1.doubanio.com/img/celebrity/large/5979.jpg\",\n" +
-            "                \"medium\": \"https://img1.doubanio.com/img/celebrity/medium/5979.jpg\"\n" +
-            "            },\n" +
-            "            \"name\": \"孙淳\",\n" +
-            "            \"id\": \"1274640\"\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"collect_count\": 171100,\n" +
-            "    \"original_title\": \"湄公河行动\",\n" +
-            "    \"subtype\": \"movie\",\n" +
-            "    \"directors\": [\n" +
-            "        {\n" +
-            "            \"alt\": \"https://movie.douban.com/celebrity/1275075/\",\n" +
-            "            \"avatars\": {\n" +
-            "                \"small\": \"https://img1.doubanio.com/img/celebrity/small/1372934445.18.jpg\",\n" +
-            "                \"large\": \"https://img1.doubanio.com/img/celebrity/large/1372934445.18.jpg\",\n" +
-            "                \"medium\": \"https://img1.doubanio.com/img/celebrity/medium/1372934445.18.jpg\"\n" +
-            "            },\n" +
-            "            \"name\": \"林超贤\",\n" +
-            "            \"id\": \"1275075\"\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"year\": \"2016\",\n" +
-            "    \"images\": {\n" +
-            "        \"small\": \"https://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2380677316.jpg\",\n" +
-            "        \"large\": \"https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2380677316.jpg\",\n" +
-            "        \"medium\": \"https://img3.doubanio.com/view/movie_poster_cover/spst/public/p2380677316.jpg\"\n" +
-            "    },\n" +
-            "    \"alt\": \"https://movie.douban.com/subject/25815034/\",\n" +
-            "    \"id\": \"25815034\"\n" +
-            "}";
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-
-        DoubanMovie movie = gson.fromJson(jsonString, DoubanMovie.class);
-        movie.getTitle();
     }
 
     @Override
@@ -167,17 +90,15 @@ public class HomeActivity extends ToolbarActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_all) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_now) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_favorite) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
